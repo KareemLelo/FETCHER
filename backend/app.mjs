300ed2da436d2b9a6b71de4c8483e6714de6b95d
zaidrawashdeh911 <zai20200907@std.psut.edu.jo>
2024-04-09 08:15:35 +0300

    using .env to call the Mongodb uri and Port number. (security concerns)

--- backend/app.mjs
+++ backend/app.mjs
@@ -7,31 +7,34 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
+const URI = process.env.MONGO_URI;
 
-const uri = "mongodb+srv://banaalhawadya:BZKMONGO@cluster0.gb0y35r.mongodb.net/BZKdatabase?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(uri);
+mongoose.connect(URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
 
-mongoose.connection.on('open', () => {
+/*mongoose.connection.on('open', () => {
     console.log('Connected to MongoDB');
 });
-  
+
 mongoose.connection.on('error', (err) => {
     console.error('MongoDB connection error:', err);
 });
+*/
 const app = express();
-
-const PORT = 3500;
 
 const corsOptions = 
 {
     origin: 'http://localhost:5173', // This is the client's URL
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-  
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/',userRoutes); //tells the Express. app to use the router defined in userRoutes for any HTTP requests that match the path '/api/users'
 // '/api/users': This is the base path or route prefix. The userRoutes router will handle any requests that start with this path.
+
+const PORT = process.env.PORT;
 
 const startServer = async () => {
     //await connectDb(); // Connect to database
