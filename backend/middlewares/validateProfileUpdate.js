abd334d54556d47f9c4d559a5f1b056562ba619b
zaidrawashdeh911 <zai20200907@std.psut.edu.jo>
2024-05-01 22:50:01 +0300

    implement validation

--- backend/middlewares/validateProfileUpdate.js
+++ backend/middlewares/validateProfileUpdate.js
@@ -1,4 +1,4 @@
-/*import User from '../models/user.js';
+import User from '../models/user.js';
 
 const validateProfileUpdate = async (req, res, next) => {
   const { userName, email, mobile } = req.body;
@@ -20,4 +20,4 @@ const validateProfileUpdate = async (req, res, next) => {
   }
 };
 
-export default validateProfileUpdate;*/
+export default validateProfileUpdate;
