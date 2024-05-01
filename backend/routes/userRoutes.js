53bd2292a92b9193035a3a4a83d837be13c1a4ba
zaidrawashdeh911 <zai20200907@std.psut.edu.jo>
2024-05-01 22:48:06 +0300

    fixing routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-//import { getUserProfile, registerUser,loginUser } from '../controllers/userController.js';
+import { getUserProfile, registerUser,loginUser, updateUserProfile} from '../controllers/userController.js';
 import validateRegistration from '../middlewares/validateRegMiddleware.js';
-//import validateProfileUpdate from '../middlewares/validateProfileUpdate.js';
-import { registerUser,loginUser } from '../controllers/userController.js';
+import validateProfileUpdate from '../middlewares/validateProfileUpdate.js';
+//import { registerUser,loginUser } from '../controllers/userController.js';
 
 //import {protectRoutes} from '../middlewares/authMiddleware.js'
 //import validateProfileUpdate from '../middlewares/validateProfileUpdate.js';
@@ -15,7 +15,7 @@ router.post('/login',loginUser);
 //protected routes
 //router.route('/profile/:_id').get(protectRoutes,getUserProfile);
 //router.route('/profile').put(protectRoutes, validateProfileUpdate, updateUserProfile);
-//router.get('/profile',getUserProfile)
-//router.post('/profile',validateProfileUpdate,updateUserProfile)
+router.get('/profile',getUserProfile)
+router.post('/profile',validateProfileUpdate,updateUserProfile)
 
 export default router;
